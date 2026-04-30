7d430750eb5a79d6ce9a08315ead6ca809132f63
eya joudi <262185128+eyajoudi@users.noreply.github.com>
2026-04-30 11:16:26 +0100

    Refactor Nom class and fix variable naming

--- Nom.java
+++ Nom.java
@@ -1,14 +1,15 @@
 public class Nom {
-        private String nom_original;
+        private String nomOriginal;
+        private List<string> nomNormalise;
 
         public Nom(String nomComplet) {
-            this.nom_original = nomComplet;
+            this.nomOriginal = nomComplet;
         }
         public int getNbMots() {
             int count = 0;
             boolean dansMot = false;
-            for (int i = 0; i < nom_original.length(); i++) {
-                char c = nom_original.charAt(i);
+            for (int i = 0; i < nomOriginal.length(); i++) {
+                char c = nomOriginal.charAt(i);
 
                 if (c != ' ') {
                     if (!dansMot) {
@@ -24,7 +25,7 @@ public class Nom {
         public int getLongueurTotal() {
             int longueur = 0;
 
-            for (int i = 0; i < nom_original.length(); i++) {
+            for (int i = 0; i < nomOriginal.length(); i++) {
                 if (nom_original.charAt(i) != ' ') {
                     longueur++;
                 }
@@ -34,11 +35,11 @@ public class Nom {
         public String getPremierMot() {
             String resultat = "";
             int i = 0;
-            while (i < nom_original.length() && nom_original.charAt(i) == ' ') {
+            while (i < nomOriginal.length() && nomOriginal.charAt(i) == ' ') {
                 i++;
             }
-            while (i < nom_original.length() && nom_original.charAt(i) != ' ') {
-                resultat += nom_original.charAt(i);
+            while (i < nomOriginal.length() && nomOriginal.charAt(i) != ' ') {
+                resultat += nomOriginal.charAt(i);
                 i++;
             }
 
@@ -46,11 +47,11 @@ public class Nom {
         }
         public String getDernierMot() {
             String resultat = "";
-            int i = nom_original.length() - 1;
-            while (i >= 0 && nom_original.charAt(i) == ' ') {
+            int i = nomOriginal.length() - 1;
+            while (i >= 0 && nomOriginal.charAt(i) == ' ') {
                 i--;
             }
-            while (i >= 0 && nom_original.charAt(i) != ' ') {
+            while (i >= 0 && nomOriginal.charAt(i) != ' ') {
                 resultat = nom_original.charAt(i) + resultat;
                 i--;
             }
