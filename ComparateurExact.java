65088087ebf466d90e65730d700e39b37a70d154
Asma Naffeti <asma.nafati@etudiant-enit.utm.tn>
2026-04-30 10:40:26 +0100

    Update ComparateurExact.java

--- ComparateurExact.java
+++ ComparateurExact.java
@@ -1,5 +1,4 @@
 public class ComparateurExact extends ResponsableComparaison {
-    @Override
     public double comparer(Nom nom1, Nom nom2) {
         return nom1.getNomNormalise().equals(nom2.getNomNormalise()) ? 1.0 : 0.0;
     }
